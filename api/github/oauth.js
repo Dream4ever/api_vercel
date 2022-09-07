06b436d736d95894f50e1a2680c3f4a78f3eb1df
Dream4ever <cnhwvista@gmail.com>
2022-09-07 10:33:08 +0800

    feat: 用 code 换取 access_token

--- api/github/oauth.js
+++ api/github/oauth.js
@@ -1,9 +1,25 @@
-export default function handler(request, response) {
+import axios from 'axios'
+
+export default async function handler(request, response) {
   const { code } = request.query
 
   if (!code) {
     return response.status(400).end()
   }
 
-  response.status(200).send(`Hello ${code}!`)
+  const url = 'https://github.com/login/oauth/access_token?' +
+  `client_id=${process.env.CLIENT_ID}&` +
+  `client_secret=${process.env.CLIENT_SECRET}&` +
+  `code=${code}`
+
+  const headers = {
+    headers: {
+      accept: 'application/json'
+    }
+  }
+
+  const result = await axios.post(url, headers)
+
+
+  response.status(200).send(result)
 }
