9d82e5025a76e306b13d8f1e4b5885daf27578c3
Dream4ever <cnhwvista@gmail.com>
2022-09-07 10:44:12 +0800

    feat: 用 access token 获取用户信息

--- api/github/oauth.js
+++ api/github/oauth.js
@@ -3,10 +3,13 @@ import axios from 'axios'
 export default async function handler(request, response) {
   const { code } = request.query
 
+  // 1. 检查 URL 查询字符串中是否有 code
   if (!code) {
     return response.status(400).end()
   }
 
+
+  // 2. 用 code 获取 access token
   const url = 'https://github.com/login/oauth/access_token?' +
   `client_id=${process.env.CLIENT_ID}&` +
   `client_secret=${process.env.CLIENT_SECRET}&` +
@@ -20,6 +23,27 @@ export default async function handler(request, response) {
 
   const result = await axios.post(url, headers)
 
+  if (result.status !== 200 || /access_token=\w+/.test(result.data)) {
+    return response.status(400).end()
+  }
 
-  response.status(200).send(result.data)
+  const accessToken = result.data.match(/access_token=(\w+)/)[1]
+
+
+
+  // 3. 用 access token 获取用户信息
+  const url2 = `https://api.github.com/user`
+  const headers2 = {
+    headers: {
+      accept: 'application/json',
+      Authorization: `token ${accessToken}`
+    }
+  }
+  const user = await axios.get(url2, headers2)
+
+  if (!user || !user.id) {
+    return response.status(400).end()
+  }
+
+  response.status(200).send(user)
 }
